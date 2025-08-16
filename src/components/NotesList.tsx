0254b48864236a164746c49adb59d9022e6dc17c
Furkan Sarı <furkansa@hotmail.com.tr>
2025-08-16 23:42:19 +0300

    style: improve inner-left-menu layout

--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -6,7 +6,19 @@ const NotesList = () => {
       {data &&
         data.map((item) => (
           <li key={item.id}>
-            <a href="#">{item.title}</a>
+            <a href={`#${item.id}`} className="note-item text-preset-sans-3">
+              {item.title}
+              <div className="item-details">
+                <div className="item-tags">
+                  {item.tags.map((tag) => (
+                    <span key={tag} className="item-tag">
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+                <span className="item-date">{item.lastEdited}</span>
+              </div>
+            </a>
           </li>
         ))}
     </ul>
