41e1d64de6ff7595c7ce5ecffc8e71a2efa31650
Furkan Sarı <furkansa@hotmail.com.tr>
2025-08-31 16:34:21 +0300

    fix: an issue with the `isLaptop` context
    this context was added for a very specific thing and it was breaking the
    layout unexpectedly. this should no longer be the case.

--- src/context/MobileContextProvider.tsx
+++ src/context/MobileContextProvider.tsx
@@ -4,8 +4,8 @@ import { useMediaQuery } from "react-responsive";
 const MobileContextProvider = ({ children }: { children: React.ReactNode }) => {
   const isMobile = useMediaQuery({ maxWidth: "425px" });
   const isTablet = useMediaQuery({ minWidth: "426px", maxWidth: "768px" });
-  const isLaptop = useMediaQuery({ minWidth: "769px", maxWidth: "1200px" });
-  const isDesktop = useMediaQuery({ minWidth: "1201px" });
+  const isLaptop = useMediaQuery({ minWidth: "769px", maxWidth: "1024px" });
+  const isDesktop = useMediaQuery({ minWidth: "769px" });
 
   return (
     <MobileContext.Provider value={{ isMobile, isTablet, isLaptop, isDesktop }}>
